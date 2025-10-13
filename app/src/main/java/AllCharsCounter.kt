232b88826f3db5da71e9f14ffa9a97c5cc1d34a6
Viktor <glebovnash12@gmail.com>
2025-10-13 21:02:47 +0300

    ALG-10: countChars

--- app/src/main/java/AllCharsCounter.kt
+++ app/src/main/java/AllCharsCounter.kt
@@ -1,16 +1,20 @@
-//fun main() {
-//
-//    val expected = mapOf('p' to 1, 'o' to 2, 'g' to 1, 'd' to 1, 'a' to 1)
-//    val actual: Map<Char, Int> = countChars(input)
-//    println("expected=$expected, actual=$actual")
-//}
-//
-//fun countChars(input: String): Map<Char, Int> {
-//    val result = mutableMapOf<Char, Int>()
-//    for (i in 0..input.lastIndex) {
-//        val char = input.get(i)
-//
-//    }
-//
-//    return result
-//}
+fun main() {
+    val input = "pogoda"
+    val expected = mapOf('p' to 1, 'o' to 2, 'g' to 1, 'd' to 1, 'a' to 1)
+    val actual: Map<Char, Int> = countChars(input)
+    println("expected=$expected, actual=$actual")
+}
+
+fun countChars(input: String): Map<Char, Int> {
+    val map = mutableMapOf<Char, Int>()
+    for (i in 0..input.lastIndex) {
+        val char = input.get(i)
+        val currentValueInMap = map.get(char)
+        if (currentValueInMap == null) {
+            map.put(char, 1)
+        } else {
+            map.put(char, currentValueInMap + 1)
+        }
+    }
+    return map
+}
