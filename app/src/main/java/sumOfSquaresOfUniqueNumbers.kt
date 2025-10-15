e19335d76ab4da4ed812249988054ce8f8eac72f
Viktor <glebovnash12@gmail.com>
2025-10-15 16:42:52 +0300

    ALG-15 sumOfSquaresOfUniqueNumbers

---
+++ app/src/main/java/sumOfSquaresOfUniqueNumbers.kt
@@ -0,0 +1,28 @@
+fun main() {
+    val input = "привет 7 мир 5 7 3" // строка с целыми числами, которые разделены пробелами
+    val actual = sumOfSquaresOfUniqueNumbers(input)
+    val exptected = listOf(9, 25, 49)
+
+    // суть (3*3) (5*5) (7*7) - берем только уникальные, отсортированный значения значения чисел в квадрате
+
+    println("actual=$actual")
+    println("exptected=$exptected")
+}
+
+fun sumOfSquaresOfUniqueNumbers(input: String): List<Int> {
+    val uniqueNumbers = mutableSetOf<Int>()
+    val parts = input.split(" ")
+
+    for (part in parts) {
+        if (part.isNotEmpty() && part.all { it.isDigit() }) {
+            uniqueNumbers.add(part.toInt())
+        }
+    }
+    return uniqueNumbers.sorted().map { it * it }
+}
+
+
+
+
+
+
