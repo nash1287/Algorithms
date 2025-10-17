fb4154c3e2a9db3fe15cbee5883378569844a0e8
Viktor <glebovnash12@gmail.com>
2025-10-17 10:52:34 +0300

    ALG-5: reverseNew

--- app/src/main/java/reverse.kt
+++ app/src/main/java/reverse.kt
@@ -4,13 +4,16 @@ fun main() {
     val expected = "reenignEAQAolleH"
     println("actual=$actual expected=$expected")
 }
-
 fun reverse(input: String): String {
-    var result = ""
-    for (i in input.lastIndex downTo  0) {
-        val char = input.get(i)
-        result += char
-    }
-
-    return result
+    val reverseText = input.reversed()
+    return reverseText
 }
+//fun reverse(input: String): String {
+//    var result = ""
+//    for (i in input.lastIndex downTo  0) {
+//        val char = input.get(i)
+//        result += char
+//    }
+//
+//    return result
+//}
