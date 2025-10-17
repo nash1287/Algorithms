8b9f8c9a5dc25c1f723965ecf98eb4a6b9c5dabd
Viktor <glebovnash12@gmail.com>
2025-10-17 08:48:08 +0300

    ALG-1: countCharNew

--- app/src/main/java/Counter.kt
+++ app/src/main/java/Counter.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val numberOfChar = countChar("hello Sasnha",'n')
+        val numberOfChar = countChar("hello Sasnha",'n')
 
     println(numberOfChar)
 }
@@ -7,15 +7,35 @@ fun main() {
 fun countChar(text: String, char: Char): Int {
     var counter = 0
 
-    for (i in 0..text.lastIndex) {
-        val _char = text.get(i)
+    for (i in text) {
+        val _char = i
 
         if (_char == char) {
             counter += 1
 
         }
     }
-
     return counter
 }
+
+
+//    val numberOfChar = countChar("hello Sasnha",'n')
+//
+//    println(numberOfChar)
+//}
+//
+//fun countChar(text: String, char: Char): Int {
+//    var counter = 0
+//
+//    for (i in 0..text.lastIndex) {
+//        val _char = text.get(i)
+//
+//        if (_char == char) {
+//            counter += 1
+//
+//        }
+//    }
+//
+//    return counter
+//}
 
