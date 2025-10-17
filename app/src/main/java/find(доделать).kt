449d932dd9990e2f89752d4bfd5d384e65081a6e
Viktor <glebovnash12@gmail.com>
2025-10-17 09:47:32 +0300

    ALG-17: findNew

--- app/src/main/java/find(доделать).kt
+++ app/src/main/java/find(доделать).kt
@@ -1,5 +1,7 @@
+import androidx.appcompat.widget.WithHint
+
 fun main() {
-    val input = listOf(3, 6, 2, 1, 4, 5, 7, 8, 10, 11)
+    val input = listOf(3, 6, 2, 1, 4, 5, 7, 8, 10, 15)
 
     val actual = find(input)
     val expected = 9
@@ -7,11 +9,20 @@ fun main() {
     println("actual=$actual expected=$expected")
 }
 
-fun find(input: List<Int>): Int {
-    input.sorted().zipWithNext().forEach { (first, second) ->
+fun find(input: List<Int>): String {
+    var missingNumber = ""
+    val sortList = input.sorted()
+    sortList.zipWithNext().forEach { (first, second) ->
         if (second - first > 1) {
-            return first + 1
+            missingNumber += first + 1
         }
     }
-    return input.sorted().last()+1
+    return missingNumber
 }
+//    input.sorted().zipWithNext().forEach { (first, second) ->
+//        if (second - first > 1) {
+//            return first + 1
+//        }
+//    }
+//    return input.sorted().last()+1
+//}
