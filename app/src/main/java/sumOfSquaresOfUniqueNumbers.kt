a4148cced6338e1165653ae37149568d61c6381d
Viktor <glebovnash12@gmail.com>
2025-10-19 14:29:38 +0300

    ALG-21 monades

--- app/src/main/java/sumOfSquaresOfUniqueNumbers.kt
+++ app/src/main/java/sumOfSquaresOfUniqueNumbers.kt
@@ -1,36 +1,37 @@
 fun main() {
-    val input = "привет 7 мир 5 7 3" // строка с целыми числами, которые разделены пробелами
+    val input =
+        "!=34 =g @привет7мир5 7 33 589" // строка с целыми числами, которые разделены пробелами
     val actual = sumOfSquaresOfUniqueNumbers(input)
-    val exptected = listOf(9, 25, 49)
+    val exptected = listOf(25, 49, 1089,1156,346921)
 
     // суть (3*3) (5*5) (7*7) - берем только уникальные, отсортированный значения значения чисел в квадрате
 
     println("actual=$actual")
     println("exptected=$exptected")
 }
+
 fun sumOfSquaresOfUniqueNumbers(input: String): List<Int> {
     return input
-        .split(" ")
-        .map {str-> str.toIntOrNull() }
-        .filterNotNull()
+        .map { char ->
+            if (char.isDigit()) {
+                char
+            } else {
+                '_'
+            }
+        }
+        .joinToString("")
+        .split('_')
+        .mapNotNull { str -> str.toIntOrNull() }
         .distinct()
         .sorted()
-        .map {it*it}
+        .map { it * it }
 }
-////fun sumOfSquaresOfUniqueNumbers(input: String): List<Int> {
-////    val uniqueNumbers = mutableSetOf<Int>()
-////    val parts = input.split(" ")
-////
-////    for (part in parts) {
-////        if (part.isNotEmpty() && part.all { it.isDigit() }) {
-////            uniqueNumbers.add(part.toInt())
-////        }
-////    }
-////    return uniqueNumbers.sorted().map { it * it }
-////}
-//
-//
-//
-//
-//
-//
+//fun sumOfSquaresOfUniqueNumbers(input: String): List<Int> {
+//    return input
+//        .split(" ")
+//        .map {str-> str.toIntOrNull() }
+//        .filterNotNull()
+//        .distinct()
+//        .sorted()
+//        .map {it*it}
+//}
