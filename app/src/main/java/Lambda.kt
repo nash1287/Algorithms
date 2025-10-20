21d739ef66db85c48d53e314630f4d2fd8d4a7f1
Viktor <glebovnash12@gmail.com>
2025-10-20 19:33:44 +0300

    lambda

--- app/src/main/java/Lambda.kt
+++ app/src/main/java/Lambda.kt
@@ -1,7 +1,20 @@
 fun main() {
-    val lambda: (Int)->Int = { number->
-        println("Hello")
-        10
+    val lambda: (Float) -> String = { number ->
+
+        "Hello"
     }
-    val n = lambda.invoke(5)
+    val n = lambda.invoke(5f)
+
+    val list = listOf(1, 2, 3)
+    val l = {}
+
+    helloFromLambda {
+
+    }
+}
+
+
+fun helloFromLambda(lam:()->Unit) :Int {
+    lam.invoke()
+    return 0
 }
