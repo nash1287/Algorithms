2474a63fe15151d06e7955499444b14b6fc24beb
Viktor <glebovnash12@gmail.com>
2025-10-23 18:42:45 +0300

    ALG-24 isValidBrackets

--- app/src/main/java/isValidBrackets.kt
+++ app/src/main/java/isValidBrackets.kt
@@ -16,11 +16,16 @@ fun main() {
     val actual4 = isValidBrackets(input4)
     val expected4 = false
 
+    val input5 = "{[)(]}"
+    val actual5 = isValidBrackets(input5)
+    val expected5 = false
+
 
     println("input1=$input1,actual1=$actual1,expected1 = $expected1")
     println("input2=$input2,actual2=$actual2,expected2 = $expected2")
     println("input3=$input3,actual3=$actual3,expected3 = $expected3")
     println("input4=$input4,actual4=$actual4,expected4 = $expected4")
+    println("input5=$input5,actual5=$actual5,expected5 = $expected5")
 
 }
 
@@ -31,12 +36,11 @@ fun isValidBrackets(input: String): Boolean {
         val char = input.get(i)
         if (char == '{') {
             counter++
-        } else if (char=='['){
+        } else if (char == '[') {
             counter++
-        } else if(char=='(') {
+        } else if (char == '(') {
             counter++
-        }
-        else {
+        } else {
             counter--
         }
         if (counter < 0) {
