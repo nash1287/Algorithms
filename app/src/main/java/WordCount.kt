8db001d6a9078276bfbeec17fc1ba58b0405ebdf
Viktor <glebovnash12@gmail.com>
2025-10-23 19:22:22 +0300

    ALG-26 wordCount

---
+++ app/src/main/java/WordCount.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val input = "hello world hello"
+    val actual = wordCount(input)
+    val expected = mapOf("hello" to 2, "world" to 1)
+    println("actual=$actual expected=$expected")
+}
+
+fun wordCount(input: String): Map<String, Int> {
+    val splitInput = input.split(" ")
+    var map = mutableMapOf<String, Int>()
+    for (word in splitInput) {
+        val number = map.get(word)
+        if (number == null) {
+            map.put(word, 1)
+        } else {
+            map.put(word, number+1)
+        }
+    }
+    return map
+}
