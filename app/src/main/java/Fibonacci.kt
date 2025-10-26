6362d465ff1bc925a09a486f26a2a6ec8fe2473f
Viktor <glebovnash12@gmail.com>
2025-10-26 10:33:42 +0300

    ALG-30 fibonacci

---
+++ app/src/main/java/Fibonacci.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val input = 6
+    val actual = fibonacci(input)
+    val expected = 8
+    println("actual=$actual expected=$expected")
+}
+//    0,1,1,2,3,5,8,13
+
+fun fibonacci(input: Int): Int {
+    val list = mutableListOf(0, 1)
+    while (list.size < input + 1) {
+        val lastNumber = list.get(list.lastIndex)
+        val previosLastNumber = list.get(list.lastIndex - 1)
+        val number = lastNumber + previosLastNumber
+        list.add(number)
+    }
+    return list.last()
+}
