0f76d6e0d0e228b89998b45386a832b7582dba92
Viktor <glebovnash12@gmail.com>
2025-10-27 19:35:22 +0300

    ALG-31 extractWords

---
+++ app/src/main/java/extractWords.kt
@@ -0,0 +1,28 @@
+fun main() {
+    val input =
+        "Hello 7 89 robot j"
+    val actual = extractWords(input)
+    val exptected = listOf("Hello, robot, j")
+
+    println("actual=$actual")
+    println("exptected=$exptected")
+}
+
+fun extractWords(input: String): List<String> {
+    return input.map { char ->
+        if (char.isDigit() || char.isWhitespace()) {
+            '_'
+        } else {
+            char
+        }
+    }
+        .joinToString("")
+
+        .split('_')
+        .filterNot {
+            it == ""
+        }
+}
+
+
+
