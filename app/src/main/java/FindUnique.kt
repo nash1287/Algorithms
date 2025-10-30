8ca86608fb24802a147e509b32d354f3d25ec93c
Viktor <glebovnash12@gmail.com>
2025-10-30 15:34:29 +0300

    ALG-28 findUniqueV2

--- app/src/main/java/FindUnique.kt
+++ app/src/main/java/FindUnique.kt
@@ -1,14 +1,14 @@
 fun main() {
-    val input = listOf(4, 5, 4, 2, 5)
+    val input = listOf(4, 5, 2, 2, 5)
     val actual = findUnique(input)
-    val expected = 2
+    val expected = 4
     println("actual=$actual expected=$expected")
 }
 fun findUnique(input: List<Int>): Int {
     val map = mutableMapOf<Int, Int>()
     input.forEach { number ->
         if (map.contains(number)) {
-            map.put(number,0)
+            map.put(number,map.getValue(number)+1)
         } else {
             map.put(number, 1)
         }
