50a4189c6affd271c64945571b306880fce28846
Viktor <glebovnash12@gmail.com>
2025-10-30 10:54:51 +0300

    ALG-15 sumOfSquaresOfUniqueNumbersRefactor

--- app/src/main/java/sumOfSquaresOfUniqueNumbers.kt
+++ app/src/main/java/sumOfSquaresOfUniqueNumbers.kt
@@ -2,7 +2,7 @@ fun main() {
     val input =
         "!=34 =g @привет7мир5 7 33 589" // строка с целыми числами, которые разделены пробелами
     val actual = sumOfSquaresOfUniqueNumbers(input)
-    val exptected = listOf(25, 49, 1089,1156,346921)
+    val exptected = listOf(1156, 49, 25, 1089, 346921)
 
     // суть (3*3) (5*5) (7*7) - берем только уникальные, отсортированный значения значения чисел в квадрате
 
@@ -10,6 +10,22 @@ fun main() {
     println("exptected=$exptected")
 }
 
+//fun sumOfSquaresOfUniqueNumbers(input: String): List<Int> {
+//    return input
+//        .map { char ->
+//            if (char.isDigit()) {
+//                char
+//            } else {
+//                '_'
+//            }
+//        }
+//        .joinToString("")
+//        .split('_')
+//        .mapNotNull { str -> str.toIntOrNull() }
+//        .distinct()
+//        .sorted()
+//        .map { it * it }
+//}
 fun sumOfSquaresOfUniqueNumbers(input: String): List<Int> {
     return input
         .map { char ->
@@ -20,18 +36,9 @@ fun sumOfSquaresOfUniqueNumbers(input: String): List<Int> {
             }
         }
         .joinToString("")
-        .split('_')
-        .mapNotNull { str -> str.toIntOrNull() }
+        .split("_")
+        .map { str -> str.toIntOrNull() }
+        .filterNotNull()
         .distinct()
-        .sorted()
         .map { it * it }
 }
-//fun sumOfSquaresOfUniqueNumbers(input: String): List<Int> {
-//    return input
-//        .split(" ")
-//        .map {str-> str.toIntOrNull() }
-//        .filterNotNull()
-//        .distinct()
-//        .sorted()
-//        .map {it*it}
-//}
