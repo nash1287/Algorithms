76114b960fddffcfa59e02a5e3514fd3b9dfd809
Viktor <glebovnash12@gmail.com>
2025-11-11 17:00:01 +0300

    ALG-37 groupProductsByCategory

--- app/src/main/java/findOccurrences.kt
+++ app/src/main/java/findOccurrences.kt
@@ -1,27 +1,27 @@
-fun main() {
-    val text = "ababa ABaBa aBa"
-    val pattern = "aba"
-
-    val actual = findOccurrences(text, pattern)
-    val expected = listOf(0, 2, 6, 8, 12)
-
-    println("actual=$actual expected=$expected")
-}
-
-fun findOccurrences (input: String, pattern: String): List<Int> {
-    val list = mutableListOf<Int>()
-    val inputLowerCase = input.lowercase()
-    var pattern = ""
-
-    for (i in 0..inputLowerCase.lastIndex) {
-        val char = inputLowerCase.get(i)
-        if (char=='a'||char=='b') {
-            pattern+=char
-            println(pattern)
-        }
-    }
-    val aba = pattern.split()
-    println(aba)
-
-    return list
-}
+//fun main() {
+//    val text = "ababa ABaBa aBa"
+//    val pattern = "aba"
+//
+//    val actual = findOccurrences(text, pattern)
+//    val expected = listOf(0, 2, 6, 8, 12)
+//
+//    println("actual=$actual expected=$expected")
+//}
+//
+//fun findOccurrences (input: String, pattern: String): List<Int> {
+//    val list = mutableListOf<Int>()
+//    val inputLowerCase = input.lowercase()
+//    var pattern = ""
+//
+//    for (i in 0..inputLowerCase.lastIndex) {
+//        val char = inputLowerCase.get(i)
+//        if (char=='a'||char=='b') {
+//            pattern+=char
+//            println(pattern)
+//        }
+//    }
+//    val aba = pattern.split()
+//    println(aba)
+//
+//    return list
+//}
