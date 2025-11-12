612793b4a3ff4adb95ea2c620303ce96e21915ee
Viktor <glebovnash12@gmail.com>
2025-11-12 16:31:12 +0300

    ALG-39 findOccurrences

--- app/src/main/java/findOccurrences.kt
+++ app/src/main/java/findOccurrences.kt
@@ -1,27 +1,22 @@
-//fun main() {
-//    val text = "ababa ABaBa aBa"
-//    val pattern = "aba"
-//
-//    val actual = findOccurrences(text, pattern)
-//    val expected = listOf(0, 2, 6, 8, 12)
-//
-//    println("actual=$actual expected=$expected")
-//}
-//
-//fun findOccurrences (input: String, pattern: String): List<Int> {
-//    val list = mutableListOf<Int>()
-//    val inputLowerCase = input.lowercase()
-//    var pattern = ""
-//
-//    for (i in 0..inputLowerCase.lastIndex) {
-//        val char = inputLowerCase.get(i)
-//        if (char=='a'||char=='b') {
-//            pattern+=char
-//            println(pattern)
-//        }
-//    }
-//    val aba = pattern.split()
-//    println(aba)
-//
-//    return list
-//}
+fun main() {
+    val text = "ababa ABaBa aBa"
+    val pattern = "aba"
+
+    val actual = findOccurrences(text, pattern)
+    val expected = listOf(0, 2, 6, 8, 12)
+
+    println("actual=$actual expected=$expected")
+}
+
+fun findOccurrences (input: String, pattern: String): List<Int> {
+    val result = mutableListOf<Int>()
+    var position = 0
+
+    while (true) {
+        val index = input.lowercase().indexOf(pattern,position)
+        if(index==-1) break
+        result.add(index)
+        position=index+1
+    }
+    return result
+}
