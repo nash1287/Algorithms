ef5d5d7ff71655c83498f73166d885c90bc28e32
Viktor <glebovnash12@gmail.com>
2025-11-20 10:54:17 +0300

    pickEvenUniques

--- app/src/main/java/pickEvenUniques.kt
+++ app/src/main/java/pickEvenUniques.kt
@@ -1,3 +1,5 @@
+import androidx.core.app.PictureInPictureModeChangedInfo
+
 fun main() {
     val input = listOf(7, 2, 4, 3, 4, 10, 9, 8, 2, 6, 1)
 
@@ -7,13 +9,24 @@ fun main() {
     println("actual=$actual expected=$expected")
 }
 
-fun pickEvenUniques(input:List<Int>):List<Int> {
-    val sortedUniqueList = input.sorted().distinct()
-    val evenNumbers = mutableListOf<Int>()
-    sortedUniqueList.forEach {
-        if (it%2 ==0) {
-            evenNumbers.add(it)
+fun pickEvenUniques(input: List<Int>): List<Int> {
+    val isEvenList = mutableListOf<Int>()
+    input.forEach { number ->
+        if (number % 2 == 0) {
+            isEvenList.add(number)
         }
     }
-    return evenNumbers
+    return isEvenList.distinct().sorted()
 }
+
+
+//fun pickEvenUniques(input:List<Int>):List<Int> {
+//    val sortedUniqueList = input.sorted().distinct()
+//    val evenNumbers = mutableListOf<Int>()
+//    sortedUniqueList.forEach {
+//        if (it%2 ==0) {
+//            evenNumbers.add(it)
+//        }
+//    }
+//    return evenNumbers
+//}
