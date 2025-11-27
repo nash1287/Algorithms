b9d1b5adaa91469466fca32a1e686d3ff67e6a76
Viktor <glebovnash12@gmail.com>
2025-11-27 10:20:58 +0300

    wordCount

--- app/src/main/java/WordCount.kt
+++ app/src/main/java/WordCount.kt
@@ -1,3 +1,5 @@
+import androidx.annotation.Nullable
+
 fun main() {
     val input = "hello world hello"
     val actual = wordCount(input)
@@ -5,18 +7,32 @@ fun main() {
     println("actual=$actual expected=$expected")
 }
 
-
-
 fun wordCount(input: String): Map<String, Int> {
-    val splitInput = input.split(" ")
-    var map = mutableMapOf<String, Int>()
-    for (word in splitInput) {
-        val number = map.get(word)
-        if (number == null) {
-            map.put(word, 1)
-        } else {
-            map.put(word, number + 1)
+    val map = mutableMapOf<String, Int>()
+    val inputSplit = input.split(" ")
+    for (word in inputSplit ) {
+        val str = map.get(word)
+        if(str==null) {
+            map.put(word,1)
+    } else {
+        map.put(word,str+1)
         }
+
     }
     return map
 }
+
+
+//fun wordCount(input: String): Map<String, Int> {
+//    val splitInput = input.split(" ")
+//    var map = mutableMapOf<String, Int>()
+//    for (word in splitInput) {
+//        val number = map.get(word)
+//        if (number == null) {
+//            map.put(word, 1)
+//        } else {
+//            map.put(word, number + 1)
+//        }
+//    }
+//    return map
+//}
