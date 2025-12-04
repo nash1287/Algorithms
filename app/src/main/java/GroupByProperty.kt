d6cda36788caf64db60471f717290e184f62b672
Viktor <glebovnash12@gmail.com>
2025-12-04 17:52:10 +0300

    groupProductsByCategory

--- app/src/main/java/GroupByProperty.kt
+++ app/src/main/java/GroupByProperty.kt
@@ -1,22 +1,27 @@
 ﻿fun main() {
-//    // Test Case 1
-//    val input1 = listOf(
-//        Product("apple", "fruit"),
-//        Product("carrot", "vegetables"),
-//        Product("banan", "fruit"),
-//        Product("potato", "vegetables"),
-//        Product("orange", "fruit")
-//    )
-//    val actual1 = groupProductsByCategory(input1)
-//    val expected1 = mapOf(
-//        "fruit" to listOf("apple", "banan", "orange"),
-//        "vegetables" to listOf("carrot", "potato")
-//    )
-//    println("Test 1: ${actual1 == expected1}")
-//    println("Actual: $actual1")
-//    println("Expected: $expected1")
-//    println()
-//}
+    // Test Case 1
+    val input1 = listOf(
+        Product("apple", "fruit"),
+        Product("carrot", "vegetables"),
+        Product("banan", "fruit"),
+        Product("potato", "vegetables"),
+        Product("orange", "fruit")
+    )
+    val actual1 = groupProductsByCategory(input1)
+    val expected1 = mapOf(
+        "fruit" to listOf("apple", "banan", "orange"),
+        "vegetables" to listOf("carrot", "potato")
+    )
+    println("Test 1: ${actual1 == expected1}")
+    println("Actual: $actual1")
+    println("Expected: $expected1")
+    println()
+}
+fun groupProductsByCategory(input:List<Product>):Map<String, List<String>> {
+    return input
+        .groupBy { it.category }
+        .mapValues { it.value.map { it.name } }
+}
 
 //fun groupProductsByCategory(products: List<Product>): Map<String, List<String>> {
 //return products
@@ -47,18 +52,18 @@
 //}
 
     // Test Case 3 (Empty list)
-    val input3 = emptyList<Product>()
-    val actual3 = groupProductsByCategory(input3)
-    val expected3 = emptyMap<String, List<String>>()
-    println("Test 3: ${actual3 == expected3}")
-    println("Actual: $actual3")
-    println("Expected: $expected3")
-}
-fun groupProductsByCategory(product: List<Product>): Map<String, List<String>> {
-    return product
-        .groupBy { it.category }
-        .mapValues { it.value.map { it.name } }
-}
+//    val input3 = emptyList<Product>()
+//    val actual3 = groupProductsByCategory(input3)
+//    val expected3 = emptyMap<String, List<String>>()
+//    println("Test 3: ${actual3 == expected3}")
+//    println("Actual: $actual3")
+//    println("Expected: $expected3")
+//}
+//fun groupProductsByCategory(product: List<Product>): Map<String, List<String>> {
+//    return product
+//        .groupBy { it.category }
+//        .mapValues { it.value.map { it.name } }
+//}
 
 data class Product(val name: String, val category: String)
 
