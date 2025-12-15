ef152a8515802a1a1ccf12890545fec0dc42af91
Viktor <glebovnash12@gmail.com>
2025-12-15 15:20:46 +0300

    countWords

--- app/src/main/java/countWords.kt
+++ app/src/main/java/countWords.kt
@@ -6,9 +6,12 @@ fun main() {
 
     println("actual=$actual expected=$expected")
 }
-fun countWords(input: List<String>):Map<String, Int> {
-    val map = mutableMapOf<String, Int>()
-    input.map{it.toLowerCase()}.forEach { word->
+
+
+fun countWords(input: List<String>): Map<String, Int> {
+    val map = mutableMapOf<String,Int>()
+    val inputLowercase = input.map{it.lowercase()}
+    inputLowercase.forEach { word->
         val value = map.get(word)
         if(value==null) {
             map.put(word,1)
@@ -18,6 +21,19 @@ fun countWords(input: List<String>):Map<String, Int> {
     }
     return map.toSortedMap()
 }
+
+//fun countWords(input: List<String>):Map<String, Int> {
+//    val map = mutableMapOf<String, Int>()
+//    input.map{it.toLowerCase()}.forEach { word->
+//        val value = map.get(word)
+//        if(value==null) {
+//            map.put(word,1)
+//        } else {
+//            map.put(word,value+1)
+//        }
+//    }
+//    return map.toSortedMap()
+//}
 
 //fun countWords(input: List<String>): Map<String, Int> {
 //    val inputLower = input.map { it.lowercase() }
